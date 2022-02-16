cf4b8b706fed04034ca9fcd34483938313b6a0ba
Hyeyeon Jeong <jhy_0219@daum.net>
2022-02-16 17:56:44 +0900

    콘소스, 구현된소스코드(example), nodejs기반빌드 성공파일(vtk)

---
+++ vtk/src/index.js
@@ -0,0 +1,8 @@
+
+//start with a simple import to verify the build worked
+//빌드가 제대로 작동하는지 확인하는 간단한 가져오기부터 시작합니다.
+
+import macro from '@kitware/vtk.js/macro';
+//import macro from 'vtk.js/Sources/macro';
+
+console.log('vtk.js imported: ', macro);
---
+++ vtk/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+//vtk.js webpack rules required
+const vtkRules = require('vtk.js/Utilities/config/dependency.js').webpack.core.rules;
+
+module.exports = {
+  entry: {
+    app: path.join(__dirname, 'src', 'index.js'),
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ].concat(vtkRules), //vtk.js webpack rules required
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    disableHostCheck: true,
+    hot: false,
+    quiet: false,
+    noInfo: false,
+    stats: {
+      colors: true,
+    },
+  },
+};
